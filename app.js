f88d6d32eb984e426b29ce21b7a92ef27d33772e
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-03-22 18:04:01 -0700

    removed unnecessary/test files and added additional comments

--- app.js
+++ app.js
@@ -7,46 +7,52 @@ const path = require('path')
 const mongoose = require('mongoose'); ///connection between JS and mongodb
 const ejsMate = require('ejs-mate'); ///allows basic boilerplate
 const methodOverride = require('method-override') ///allows http verbs other than POST/GET in forms 
-const catchAsync = require('./utils/catchAsync.js')
-const ExpressError = require('./utils/ExpressError')
+const catchAsync = require('./utils/catchAsync.js') ///for handling async route errors
+const ExpressError = require('./utils/ExpressError') ///general error handler middleware
 const mongoSanitize = require('express-mongo-sanitize') ///for preventing mongo injection
 const ContactInfo = require('./models/contactInfo') //require mongoose model campground.js
 
+///creates portfolio database (or connects to it if already made)
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/portfolio';
 mongoose.connect(dbUrl, {
     useNewUrlParser:true,
     useUnifiedTopology:true
-});///creates db yelp-camp(or connects to it if already made)
+});
 
+///checks to see if successfully connected to db and handles db connection error
 const db = mongoose.connection; 
 db.on("error", console.error.bind(console, "connection error:")); 
 db.once("open", () => {
     console.log("Database Connected")
-});///checks to see if connected and handles db connection error
+});
 
-const app = express(); ///starts express app 
+///starts express app 
+const app = express();
 
-//app.engine('ejs', ejsMate); ///allows basic boilerplate
-app.set('view engine', 'ejs');  ///sets view engine to ejs 
-app.set('views', path.join(__dirname, 'views')) ///so we can run app.js from outside of yelpcamp folder 
+///sets view engine to ejs 
+app.set('view engine', 'ejs');  
 
+///so we can run app.js from outside of yelpcamp folder 
+app.set('views', path.join(__dirname, 'views')) 
+
+///sets path for static files
 app.use(express.static(path.join(__dirname, 'public'))); 
-app.use(express.urlencoded({extended:true})) ///allows us to get req.params 
-app.use(methodOverride('_method')) ///allows requests other than get/post thru forms 
-app.use(mongoSanitize()) ///prevents users from inputting characters that could result in mongo injection
 
+ ///allows us to get req.params 
+app.use(express.urlencoded({extended:true}))
 
-app.get('/test2', (req,res) =>{
-    res.render('test2')
-})
+///allows requests other than get/post thru forms 
+app.use(methodOverride('_method')) 
 
+///prevents users from inputting characters that could result in mongo injection
+app.use(mongoSanitize()) 
+
+///Main route
 app.get('/', (req,res) =>{
     res.render('portfolio')
 })
-app.get('/design', (req,res) =>{
-    res.render('design')
-})
 
+///creates a ContactInfo mongoose object from form data and saves it to database
 app.post('/contact', catchAsync(async(req,res) =>{
     const contact = new ContactInfo(req.body);
     console.log(contact);
@@ -54,20 +60,23 @@ app.post('/contact', catchAsync(async(req,res) =>{
     res.redirect('/')
 }))
 
-app.all('*', (req,res,next) => { ///runs for all unrecognized urls 
-    next(new ExpressError('Page Not Found', 404))
+///runs for all unrecognized urls 
+app.all('*', (req,res,next) => {
     ///passes ExpressError into err param for app.use
+    next(new ExpressError('Page Not Found', 404))
 })
 
 ///this runs if catchAsync catches error and calls next() OR if next(new ExpressError) gets called OR if validation error 
 app.use((err,req,res,next) => { 
-    const {status = 500} = err; ///gets status and message from ExpressError passed as err, else set defaults
-    if(!err.message) err.message = 'Something went wrong!' ///if no error message, set default 
-    res.status(status).render('error',{err})
+    ///gets status and message from ExpressError passed as err, else set defaults
+    const {status = 500} = err; 
+     ///if no error message, set default 
+    if(!err.message) err.message = 'Something went wrong!'
     ///sets response status property to status passed in and renders error template 
+    res.status(status).render('error',{err})
 })
 
-///listen on heroku specified port or 3000 in dev environment 
+///listen on .env specified port or 3000 in dev environment 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>{ 
     console.log(`Serving on port ${port}`)
